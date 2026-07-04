d08d2130856fabe2c7995185b187b86b99cf7228
ankit gill <gillankit076@gmail.com>
2026-07-04 22:28:09 +0530

    feat(app): configure global application providers
    * Integrate Redux Provider with the application store
    * Add UserProvider for user state management
    * Add AppStateProvider for shared application state
    * Configure RouterProvider with centralized routing
    * Establish the application's global provider hierarchy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import { RouterProvider } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import { AppStateProvider } from './context/AppStateContext';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import router from './routes/routes';
 
 
 function App() {
   return (
-    <UserProvider>
-      <AppStateProvider>
-        <RouterProvider router={router} />
-      </AppStateProvider>
-    </UserProvider>
+    <Provider store={store}>
+      <UserProvider>
+        <AppStateProvider>
+          <RouterProvider router={router} />
+        </AppStateProvider>
+      </UserProvider>
+    </Provider>
   );
 }
 
