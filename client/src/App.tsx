11a2d84275780baf14c23ae3e34bd0bc223a1b59
ankit gill <gillankit076@gmail.com>
2026-07-13 14:33:37 +0530

    Configured global providers for theme, routes, and redux.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,21 @@
 import { RouterProvider } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import { AppStateProvider } from './context/AppStateContext';
+import { ThemeProvider } from './context/ThemeContext';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import router from './routes/routes';
 
-
 function App() {
   return (
     <Provider store={store}>
-      <UserProvider>
-        <AppStateProvider>
-          <RouterProvider router={router} />
-        </AppStateProvider>
-      </UserProvider>
+      <ThemeProvider>
+        <UserProvider>
+          <AppStateProvider>
+            <RouterProvider router={router} />
+          </AppStateProvider>
+        </UserProvider>
+      </ThemeProvider>
     </Provider>
   );
 }
