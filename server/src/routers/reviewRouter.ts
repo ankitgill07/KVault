6f082c84674116178d02d1899b27749f325c378c
ankit gill <gillankit076@gmail.com>
2026-07-15 12:11:05 +0530

    Secured review POST/PUT/DELETE routes using the authenticate middleware.

--- server/src/routers/reviewRouter.ts
+++ server/src/routers/reviewRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as reviewController from "../controllers/reviewController.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = Router();
-router.post("/", reviewController.createReview);
-router.get("/{:courseId}/reviews", reviewController.getReviewsByCourse);
-router.get("/{:id}", reviewController.getReviewById);
-router.put("/{:id}", reviewController.updateReview);
-router.delete("/{:id}", reviewController.deleteReview);
+router.post("/", authenticate, reviewController.createReview);
+router.get("/course/:courseId", reviewController.getReviewsByCourse);
+router.get("/:id", reviewController.getReviewById);
+router.put("/:id", authenticate, reviewController.updateReview);
+router.delete("/:id", authenticate, reviewController.deleteReview);
 
 
 
